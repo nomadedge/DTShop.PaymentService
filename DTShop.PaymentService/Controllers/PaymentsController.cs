470a76fc2e89f144d633f76148dc3f045dbbd466
nomadedge <nomad.edge@gmail.com>
2019-11-16 13:05:05 +0300

    add trycatch to controller

--- DTShop.PaymentService/Controllers/PaymentsController.cs
+++ DTShop.PaymentService/Controllers/PaymentsController.cs
@@ -28,29 +28,31 @@ namespace DTShop.PaymentService.Controllers
         [HttpPut("{orderId}")]
         public async Task<ActionResult<OrderModel>> PayForOrder(int orderId, UserDetailsModel userDetails)
         {
-            _logger.LogInformation("{Username} has started payment for the order with OrderId {OrderId}.",
-                userDetails.Username, orderId);
-
-            CardAuthorizationInfo cardAuthorizationInfo;
-            switch (userDetails.CardAuthorizationInfo.ToLower())
+            try
             {
-                case "authorized":
-                    cardAuthorizationInfo = CardAuthorizationInfo.Authorized;
-                    break;
-                case "unauthorized":
-                    cardAuthorizationInfo = CardAuthorizationInfo.Unauthorized;
-                    break;
-                default:
-                    return BadRequest("CardAuthorizationInfo is not valid.");
-            }
+                _logger.LogInformation("{Username} has started payment for the order with OrderId {OrderId}.",
+                    userDetails.Username, orderId);
 
-            //Should get an order from OrderService
-            var order = new OrderModel
-            {
-                OrderId = orderId,
-                Username = "Misha",
-                Status = "Collecting",
-                OrderItems = new List<OrderItemModel> { new OrderItemModel
+                CardAuthorizationInfo cardAuthorizationInfo;
+                switch (userDetails.CardAuthorizationInfo.ToLower())
+                {
+                    case "authorized":
+                        cardAuthorizationInfo = CardAuthorizationInfo.Authorized;
+                        break;
+                    case "unauthorized":
+                        cardAuthorizationInfo = CardAuthorizationInfo.Unauthorized;
+                        break;
+                    default:
+                        return BadRequest("CardAuthorizationInfo is not valid.");
+                }
+
+                //Should get an order from OrderService
+                var order = new OrderModel
+                {
+                    OrderId = orderId,
+                    Username = "Misha",
+                    Status = "Collecting",
+                    OrderItems = new List<OrderItemModel> { new OrderItemModel
                 {
                     Item = new ItemModel
                     {
@@ -60,49 +62,54 @@ namespace DTShop.PaymentService.Controllers
                     },
                     Amount = 3
                 } }
-            };
+                };
 
-            if (order.Username != userDetails.Username)
-            {
-                return BadRequest("Usernames in order and user details should be equal.");
+                if (order.Username != userDetails.Username)
+                {
+                    return BadRequest("Usernames in order and user details should be equal.");
+                }
+
+                if (order.Status.ToLower() != "collecting")
+                {
+                    return BadRequest("Order status should be \"Collecting\".");
+                }
+
+                var orderAfterPayment = order;
+                switch (cardAuthorizationInfo)
+                {
+                    case CardAuthorizationInfo.Authorized:
+                        orderAfterPayment.PaymentId = DateTime.Now.Ticks;
+                        orderAfterPayment.Status = "Paid";
+                        break;
+                    case CardAuthorizationInfo.Unauthorized:
+                        orderAfterPayment.PaymentId = DateTime.Now.Ticks;
+                        orderAfterPayment.Status = "Failed";
+                        break;
+                }
+
+                var payment = new Payment
+                {
+                    PaymentId = orderAfterPayment.PaymentId.Value,
+                    OrderId = orderAfterPayment.OrderId,
+                    Username = orderAfterPayment.Username,
+                    TotalCost = orderAfterPayment.TotalCost,
+                    IsPassed = orderAfterPayment.Status == "Paid" ? true : false
+                };
+
+                await _paymentRepository.AddPayment(payment);
+
+                _logger.LogInformation("{Username} has finished payment for the order with OrderId {OrderId} with status {Status}.",
+                    orderAfterPayment.Username, orderAfterPayment.OrderId, orderAfterPayment.Status);
+
+                //Call OrderService's method and pass orderId and paymentId to it
+                //This method will return OrderModel instance with actual data
+
+                return orderAfterPayment;
             }
-
-            if (order.Status.ToLower() != "collecting")
+            catch (Exception e)
             {
-                return BadRequest("Order status should be \"Collecting\".");
+                return BadRequest(e.Message);
             }
-
-            var orderAfterPayment = order;
-            switch (cardAuthorizationInfo)
-            {
-                case CardAuthorizationInfo.Authorized:
-                    orderAfterPayment.PaymentId = DateTime.Now.Ticks;
-                    orderAfterPayment.Status = "Paid";
-                    break;
-                case CardAuthorizationInfo.Unauthorized:
-                    orderAfterPayment.PaymentId = DateTime.Now.Ticks;
-                    orderAfterPayment.Status = "Failed";
-                    break;
-            }
-
-            var payment = new Payment
-            {
-                PaymentId = orderAfterPayment.PaymentId.Value,
-                OrderId = orderAfterPayment.OrderId,
-                Username = orderAfterPayment.Username,
-                TotalCost = orderAfterPayment.TotalCost,
-                IsPassed = orderAfterPayment.Status == "Paid" ? true : false
-            };
-
-            await _paymentRepository.AddPayment(payment);
-
-            _logger.LogInformation("{Username} has finished payment for the order with OrderId {OrderId} with status {Status}.",
-                orderAfterPayment.Username, orderAfterPayment.OrderId, orderAfterPayment.Status);
-
-            //Call OrderService's method and pass orderId and paymentId to it
-            //This method will return OrderModel instance with actual data
-
-            return orderAfterPayment;
         }
     }
 }
